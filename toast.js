1bd6edc7f7d3e087530b5d79e1f3b4f8a3a07716
Otto Lu <llw33333@gmail.com>
2015-08-28 20:37:38 +0800

    quick fix

--- toast.js
+++ toast.js
@@ -1,9 +1,31 @@
-/*! Copyright (c) 2013 Otto Lu (http://nomi.sinaapp.com)
- * Licensed under the MIT License.
+/*! Copyright (c) 2015 Otto Lu
+ * Licensed under the WTFPL, Version 2.0 (the "License"); 
+ * You may obtain a copy of the License at
+ *
+ *   http://www.wtfpl.net/about/
+ *
+ *         DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+ *                   Version 2, December 2004 
+ *
+ * Copyright (C) 2004 Sam Hocevar <sam@hocevar.net> 
+ *
+ * Everyone is permitted to copy and distribute verbatim or modified 
+ * copies of this license document, and changing it is allowed as long 
+ * as the name is changed. 
+ *
+ *           DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
+ *  TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION 
+ *
+ * 0. You just DO WHAT THE FUCK YOU WANT TO.
+ *=====================================================================
  *
  * A Simple Android Style MessageBox(Toast)
  *
- * @ Version: 0.005
+ * @ Version: 0.01
+ *
+ * @ Author:
+ *     Otto Lu (llw33333@gmail.com)
+ *     fcjy (LF@fcjy.ory)
  *
  * @ Example:
  *     You should call
@@ -15,13 +37,25 @@
  *     Toast({text: "hello, world",
  *           time: 2000,
  *           autoPos: false,
- *           x: function(){ return 100;},
+ *           x: function(){ return 100; },
  *           y: 200 }).show();
+ *
+ * @ History:
+ *   2013-3-17 (Version 0.005):
+ *     1.Intial release.
+ *
+ *   2015-8-28 (Version 0.01):
+ *     1.Add messages queue support.
+ *     2.Bugfix.
+ *
+ * @ TODO-list:
+ *     1.Code refactoring.
+ *     2.Rethink how to implement queue better.
  */
 ;(function(exports, $, undefined) {
 
-	var toastQueue = [];
-	var isWorked = false;
+	var _ToastQueue = [];
+	var isShowing = false;
 
 	var Toast = function(opt) {
 
@@ -102,10 +136,22 @@
 			}
 		})();
 
+		function toastTryShow(){
+			if (isShowing || 0 == _ToastQueue.length) {
+				return;
+			} else {
+				isShowing = true;
+			}
+			
+			setTimeout(function() {
+				_ToastQueue.shift()._show();
+			}, 300);
+		}
+
 		return {
 			show: function() {
-				toastQueue.push(this);
-				toastTryWork();
+				_ToastQueue.push(this);
+				toastTryShow();
 			},
 			
 			_show: function(ControlUrself) {
@@ -114,9 +160,9 @@
 				setTimeout(function() {
 					$ele.fadeOut(function() {
 						$ele.remove();
-						isWorked = false;
-						
-						toastTryWork();
+
+						isShowing = false;
+						toastTryShow();
 					});
 				}, options.time);
 			},
@@ -156,14 +202,6 @@
 
 	Toast.init = function() {
 		$('body').append('<style type="text/css">.toast-box {display: none;padding: 15px 30px;z-index: 999;position: fixed;-moz-border-radius: 5px;-webkit-border-radius: 5px;border-radius: 5px;background: #000;filter: alpha(opacity=60);_background: #575757;background: #000\0;filter: alpha(opacity=60)\0;background: rgba(0, 0, 0, 0.6);}.toast-box p {color: white;color: rgba(255,255,255,.9);font-size: 16px;text-align: center;width: 100%;display: block;margin: 0 auto;}.toast-box img {margin: 10px;} .toast-box.no-border-radius {-moz-border-radius: 0px;-webkit-border-radius: 0px;border-radius: 0px;}.toast-box.fixedTop,.toast-box.fixedBottom{width:100% !important;max-width:100% !important;}</style>');
-	}
-	
-	function toastTryWork(){
-		if(isWorked || !toastQueue.length)
-			return;
-		else
-			isWorked = true;
-		toastQueue.shift()._show();
 	}
 
 	exports.Toast = Toast;
